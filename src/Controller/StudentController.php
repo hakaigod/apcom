0b7a9c660bdc253a1161b06eb9c225150ed2bb39
nasunin1 <oic.a.tokuyama@gmail.com>
2017-06-08 13:14:43 +0900

    Create StudentController, index.ctp

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\Network\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+
+class StudentController extends AppController
+{
+    public function initialize(){
+        parent::initialize();
+
+    }
+
+    public function index(){
+
+    }
+}
