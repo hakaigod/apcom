e0c732a7ec0fe256052b6015b3991635ec5fbbde
odenkun <mykoic@yahoo.co.jp>
2017-06-18 23:39:04 +0900

    ggg

---
+++ src/Model/Entity/MfQe.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * MfQe Entity
+ *
+ * @property string $qesnum
+ * @property int $exanum
+ * @property string $fienum
+ * @property string $question
+ * @property string $answer_pic
+ * @property string $choice1
+ * @property string $choice2
+ * @property string $choice3
+ * @property string $choice4
+ * @property int $answer
+ */
+class MfQe extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        '*' => true,
+        'qesnum' => false,
+        'exanum' => false
+    ];
+}
---
+++ src/Model/Table/MfQesTable.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * MfQes Model
+ *
+ * @method \App\Model\Entity\MfQe get($primaryKey, $options = [])
+ * @method \App\Model\Entity\MfQe newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\MfQe[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\MfQe|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\MfQe patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\MfQe[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\MfQe findOrCreate($search, callable $callback = null, $options = [])
+ */
+class MfQesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('mf_qes');
+        $this->setDisplayField('qesnum');
+        $this->setPrimaryKey(['qesnum', 'exanum']);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->allowEmpty('qesnum', 'create');
+
+        $validator
+            ->integer('exanum')
+            ->allowEmpty('exanum', 'create');
+
+        $validator
+            ->requirePresence('fienum', 'create')
+            ->notEmpty('fienum');
+
+        $validator
+            ->allowEmpty('question');
+
+        $validator
+            ->allowEmpty('answer_pic');
+
+        $validator
+            ->allowEmpty('choice1');
+
+        $validator
+            ->allowEmpty('choice2');
+
+        $validator
+            ->allowEmpty('choice3');
+
+        $validator
+            ->allowEmpty('choice4');
+
+        $validator
+            ->integer('answer')
+            ->allowEmpty('answer');
+
+        return $validator;
+    }
+}
